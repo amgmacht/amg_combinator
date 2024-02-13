0437cedea6139f14c1c825891144caeb4e7dfed7
amgmacht <68996140+amgmacht@users.noreply.github.com>
2024-02-13 16:41:22 +0100

    some remove

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,3 @@
-shared_script '@WaveShield/resource/waveshield.lua' --this line was automatically written by WaveShield
-
-
-
- --this line was automatically written by WaveShield
-
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'

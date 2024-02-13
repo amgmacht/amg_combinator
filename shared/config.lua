da717ee14603e6c078a14abf9a5e534dfa2edc35
amgmacht <68996140+amgmacht@users.noreply.github.com>
2024-02-13 23:06:40 +0100

    Update 2.0.0

--- shared/config.lua
+++ shared/config.lua
@@ -1,28 +1,28 @@
 Config = {}
 ----------------------------------------------------------------------
 ----------------------------------------------------------------------
-Config.Progressbar = 'ox_lib'
+Config.Progressbar = 'ox_lib' -- esx/ox_lib
 Config.KombiDauer = 4000 -- Progressbar Zeit
 Config.KombiText = 'Kombinieren..' -- Progressbar Text
-Config.KombiPosition = 'middle' -- Progressbar Position
+Config.KombiPosition = 'middle' -- Progressbar Position ONLY ox_lib
 ----------------------------------------------------------------------
 ----------------------------------------------------------------------
 Config.Combinations = {
 
-    ['pizza_kaese'] = {
+    ['pizza_kaese'] = { -- Das Ziehende Item
         needs = {
             {
-                item = 'pizza_karton',
+                item = 'pizza_karton', -- Benötigte Items
                 amount = 1
             },
         },
         result = {
             [1] = {
-                name = 'pizza_karton_kaese',
+                name = 'pizza_karton_kaese', -- Erhaltene Items
                 amount = 1 
             },
         }, 
-        firstcombiremove = true,
-        needcombiremove = true, 
+        firstcombiremove = true, -- Entfernt Item
+        needcombiremove = true, -- Entfernt Item
     },
 }
